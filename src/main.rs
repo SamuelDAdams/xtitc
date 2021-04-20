c6c55b418f63e6454813f195ebed8e88b24f3b8d
Samuel Adams <samuel.adams.d@gmail.com>
2021-04-20 13:45:49 -0700

    Preprocessing done

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+mod util;
+use std::{error::Error};
+use util::*;
+
+#[derive(Default)]
+pub struct Context {
+    pub instance_count: usize,
+    pub class_label_count: usize,
+    pub original_attr_count: usize,
+    pub attribute_count: usize,
+    pub feature_count: usize,
+    pub bin_count: usize,
+    pub tree_count: usize,
+    pub max_depth: usize,
+    pub epsilon: f64,
+    pub decimal_precision: usize,
+    pub seed: usize,
+}
+
+fn main() {
+    let fileloc = "settings/settings1.toml";
+    //load settings
+    let (ctx, data, classes) = init(&fileloc.to_string()).unwrap();
+    let (disc_data, feature_selectors, feature_values) = xt_preprocess(&data, &ctx).unwrap();
+    let 
+    //preprocess dataset according to the settings
+    
+}
+
+pub fn init(cfg_file: &String) -> Result<(Context, Vec<Vec<f64>>, Vec<Vec<f64>>), Box<dyn Error>> {
+	let mut settings = config::Config::default();
+    settings
+        .merge(config::File::with_name(cfg_file.as_str())).unwrap()
+        .merge(config::Environment::with_prefix("APP")).unwrap();
+
+    let class_label_count: usize = settings.get_int("class_label_count")? as usize;
+    let attribute_count: usize = settings.get_int("attribute_count")? as usize;
+    let instance_count: usize = settings.get_int("instance_count")? as usize;
+    let feature_count: usize = settings.get_int("feature_count")? as usize;
+    let tree_count: usize = settings.get_int("tree_count")? as usize;
+    let max_depth: usize = settings.get_int("max_depth")? as usize;
+    let seed: usize = settings.get_int("seed")? as usize;
+    let epsilon: f64 = settings.get_int("epsilon")? as f64;
+    let decimal_precision: usize = settings.get_int("decimal_precision")? as usize;
+    let original_attr_count = attribute_count;
+    let bin_count = 2usize;
+
+    let data = matrix_csv_to_float_vec(&settings.get_str("data")?)?;
+    let data = data.iter().map(|x| x.iter().map(|y| truncate(y, decimal_precision).unwrap()).collect()).collect();
+    let mut classes = matrix_csv_to_float_vec(&settings.get_str("classes")?)?;
+
+    classes = transpose(&classes)?;
+    classes = classes.iter().map(|x| x.iter().map(|y| truncate(y, decimal_precision).unwrap()).collect()).collect();
+
+
+    let c = Context {
+        instance_count,
+        class_label_count,
+        attribute_count,
+        feature_count,
+        original_attr_count,
+        bin_count,
+        tree_count,
+        max_depth,
+        epsilon,
+        decimal_precision,
+        seed,
+    };
+
+    Ok((c, data, classes))
+}
+
+pub fn xt_preprocess(data: &Vec<Vec<f64>>, ctx: &Context) -> Result<(Vec<Vec<Vec<usize>>>, Vec<Vec<usize>>, Vec<Vec<f64>>), Box<dyn Error>>{
+    let maxes: Vec<f64> = data.iter().map(|x| x.iter().cloned().fold(0./0., f64::max)).collect();
+    let mins: Vec<f64> = data.iter().map(|x| x.iter().cloned().fold(1./0., f64::min)).collect();
+    let ratios = get_ratios(ctx.feature_count * ctx.tree_count, ctx.decimal_precision, ctx.seed)?;
+    let ranges: Vec<f64> = maxes.iter().zip(mins.iter()).map(|(max , min)| max - min).collect();
+    let features = get_features(ctx.feature_count * ctx.tree_count, ctx.attribute_count, ctx.seed)?;
+    let mut sel_vals = vec![];
+    let mut structured_features = vec![];
+    for i in 0 .. ctx.tree_count {
+        let mut vals = vec![];
+        let mut feats = vec![];
+        for j in 0 .. ctx.feature_count {
+            let feature = features[i * ctx.feature_count + j];
+            let ratio = ratios[i * ctx.feature_count + j];
+            vals.push(truncate(&(ranges[feature] * ratio + mins[feature]), ctx.decimal_precision)?);
+            feats.push(feature);
+        }
+        sel_vals.push(vals);
+        structured_features.push(feats);
+    }
+    let mut disc_subsets = vec![];
+    for i in 0 .. ctx.tree_count {
+        let mut disc_set = vec![];
+        for j in 0 .. ctx.feature_count {
+            let val = sel_vals[i][j];
+            let feat = structured_features[i][j];
+            let col = data[feat].iter().map(|x| (*x >= val) as usize).collect::<Vec<usize>>();
+            disc_set.push(col);
+        }
+        disc_subsets.push(disc_set);
+    }
+    Ok((disc_subsets, structured_features, sel_vals))
+}
---
+++ src/util.rs
@@ -0,0 +1,64 @@
+extern crate csv;
+
+use fixed::{types::extra::U10, FixedI64};
+use std::{error::Error};
+use std::fs::File;
+use std::fs;
+use rand::*;
+
+pub fn matrix_csv_to_float_vec(filename: &str) -> Result<Vec<Vec<f64>>, Box<dyn Error>> {
+
+    let file = File::open(filename)?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)    
+        .from_reader(file);
+
+    let mut vec: Vec<Vec<f64>> = Vec::new(); 
+
+    for entry in rdr.records() {
+
+        let row: Vec<String> = entry?.deserialize(None)?;
+        let row = row.iter().map(|e| e.parse::<f64>().unwrap()).collect::<Vec<f64>>();
+        vec.push(row);
+
+    }
+    Ok(vec)
+
+}
+
+pub fn transpose(mat: &Vec<Vec<f64>>) -> Result<Vec<Vec<f64>>, Box<dyn Error>> {
+
+    let mut mat_t = vec![vec![0f64; mat.len() ] ; mat[0].len()];
+
+    for i in 0..mat.len() {
+        for j in 0..mat[0].len() {
+            mat_t[j][i] = mat[i][j] 
+        }
+    }
+
+    Ok(mat_t)
+
+}
+
+pub fn truncate(x: &f64, decimal_precision: usize) -> Result<f64, Box<dyn Error>> {
+    let val = (x * (decimal_precision as f64)).round() / (decimal_precision as f64);
+    Ok(val)
+}
+
+pub fn get_ratios(num: usize, decimal_precision: usize, seed: usize) -> Result<Vec<f64>, Box<dyn Error>> {
+    let mut rng = if seed > 0 {rand::StdRng::from_seed(&[seed])} else {rand::StdRng::new()?};
+    let mut result = vec![];
+    for _i in 0 .. num {
+        result.push(truncate(&rng.gen_range(0f64, 1f64), decimal_precision)?);
+    }
+    Ok(result)
+}
+
+pub fn get_features(num: usize, attribute_count: usize, seed: usize) -> Result<Vec<usize>, Box<dyn Error>> {
+    let mut rng = if seed > 0 {rand::StdRng::from_seed(&[seed])} else {rand::StdRng::new()?};
+    let mut result = vec![];
+    for _i in 0 .. num {
+        result.push(rng.gen_range(0, attribute_count));
+    }
+    Ok(result)
+}
